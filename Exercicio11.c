27a36228e33b6a1dd2754b3a10370724bfccf637
Gabriel-Aralli <gabrielaralli@gmail.com>
2026-09-03 21:51:12 -0300

    Add files via upload

---
+++ Exercicio11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+
+{
+    float custo, convite, total_ingressos;
+
+    printf ("Digite o valor do custo do espetaculo: R$");
+    scanf ("%f", &custo);
+    printf ("Digite o preco por convite para o espetaculo: R$");
+    scanf ("%f", &convite);
+
+    total_ingressos = custo / convite;
+
+    printf ("O total de convites que precisam ser vendidos para abater o custo do espetaculo e de %.0f", total_ingressos);
+
+    return(0);
+}
